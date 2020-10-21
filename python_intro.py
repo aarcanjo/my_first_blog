0d8c3ef04ff9397fdf30709d487f937461a7bf14
aarcanjo <a.arcanjo@gmail.com>
2020-10-21 15:22:45 -0300

    Minha aplicacao Django Girls, first commit

---
+++ python_intro.py
@@ -0,0 +1,32 @@
+# print('Hello, ProgramaMenina!')
+
+'''if 3>5:
+    print('Funciona!')
+else:
+    print('três é menor que 5')
+
+name = "Leticia"
+
+if name == 'Arcanjo':
+    print('Oi, Arcanjo!')
+elif name == 'Izabela':
+    print('Oi, Iza!')
+else:
+    print('Olá, desconhecida!')'''
+
+def hi():
+    print('Oi!')
+    print('Tudo bem?')
+
+
+#hi()
+
+#hi('Arcanjo')
+
+def hello(name):
+    print('Olá, ', name)
+    print('Tudo bem?')
+
+hi()
+hello('Arcanjo')
+hello('Leticia')
